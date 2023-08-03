5d4e4220bc5663c616211270daf2608b60b56cb4
wangjona9 <118570694+wangjona9@users.noreply.github.com>
2023-08-02 20:43:17 -0500

    Added menu & itemSpawn changes
    Created a menu and made changes to Vegetable Spawn Script

--- MaMadnessGame/Assets/Scripts/VegetableSpawn.cs
+++ MaMadnessGame/Assets/Scripts/VegetableSpawn.cs
@@ -6,16 +6,31 @@ public class VegetableSpawn : MonoBehaviour
 {
     public GameObject itemPrefab;
     public Transform spawnContainer;
-    
+
+    private GameObject spawnedItem;
+
+    void Start()
+    {
+        // Call SpawnItem function repeatedly with a delay of 2 seconds (2.0f)
+        InvokeRepeating("SpawnItem", 2.0f, 2.0f);
+    }
+
     void SpawnItem()
     {
-        GameObject newItem = Instantiate(itemPrefab, spawnContainer);
+        spawnedItem = Instantiate(itemPrefab, spawnContainer);
         // Set the position of the newItem if needed
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.M))
-            SpawnItem(); 
+        if (spawnedItem != null)
+        {
+            // Check if the spawned item has reached the x value of -25
+            if (spawnedItem.transform.position.x <= -25f)
+            {
+                // Destroy the item if it reaches -25
+                Destroy(spawnedItem);
+            }
+        }
     }
 }
